a7400d1680640ecb4c809191d16520b2ec6f7e00
Karnbir S <karnbirsaini@hotmail.com>
2020-04-15 17:13:07 -0400

    cleanup

--- lmr.js
+++ lmr.js
@@ -1,16 +1,7 @@
 (function () {
-    // The width and height of the captured photo. We will set the
-    // width to the value defined here, but the height will be
-    // calculated based on the aspect ratio of the input stream.
-    var width = 320; // We will scale the photo width to this
-    var height = 0; // This will be computed based on the input stream
-
-    // |streaming| indicates whether or not we're currently streaming
-    // video from the camera. Obviously, we start at false.
+    var width = 320;
+    var height = 0;
     var streaming = false;
-
-    // The various HTML elements we need to configure or control. These
-    // will be set by the startup() function.
     var video = null;
     var canvas = null;
     var photo = null;
@@ -21,7 +12,6 @@
         canvas = document.getElementById('canvas');
         photo = document.getElementById('photo');
         startbutton = document.getElementById('startbutton');
-
         navigator.mediaDevices.getUserMedia({
                 video: true,
                 audio: false
@@ -37,10 +27,6 @@
         video.addEventListener('canplay', function (ev) {
             if (!streaming) {
                 height = video.videoHeight / (video.videoWidth / width);
-
-                // Firefox currently has a bug where the height can't be read from
-                // the video, so we will make assumptions if this happens.
-
                 if (isNaN(height)) {
                     height = width / (4 / 3);
                 }
@@ -60,10 +46,7 @@
 
         clearphoto();
     }
-
-    // Fill the photo with an indication that none has been
-    // captured.
-
+    
     function clearphoto() {
         var context = canvas.getContext('2d');
         context.fillStyle = "#AAA";
@@ -73,11 +56,6 @@
         photo.setAttribute('src', data);
     }
 
-    // Capture a photo by fetching the current contents of the video
-    // and drawing it into a canvas, then converting that to a PNG
-    // format data URL. By drawing it on an offscreen canvas and then
-    // drawing that to the screen, we can change its size and/or apply
-    // other changes before drawing it.
 
     function takepicture() {
         var context = canvas.getContext('2d');
@@ -93,7 +71,5 @@
         }
     }
 
-    // Set up our event listener to run the startup process
-    // once loading is complete.
     window.addEventListener('load', startup, false);
 })();
